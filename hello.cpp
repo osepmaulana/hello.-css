86eea72f51d668aec52a095c2d28fa87ec514346
osepmaulana <111963846+osepmaulana@users.noreply.github.com>
2022-08-31 15:40:14 +0700

    Add files via upload
    This my school project

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Hello word"<< endl;
+    cout << "Nama : Osep maulana"<< endl;
+    cout << "Kelas : X PPLG 1 "<< endl;
+    cout << "================="<< endl;
+    cout << "Angka1 = 10"<< endl;
+    cout << "Angka2 = 5"<< endl;
+    cout << "hasil:"<< 10+5 <<endl;
+    cout << 
+    return 0;
+}
